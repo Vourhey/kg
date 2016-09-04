2d0d7965c50e8f027826c4b35a5b02f6be368d77
Vadim Manaenko <vadim.razorq@gmail.com>
2016-09-04 17:08:14 +0300

    Escape string error

--- add.php
+++ add.php
@@ -3,6 +3,7 @@ if(!empty($_POST['addfilm'])) {
   require_once ('Snoopy.class.php');
   require_once('core.php');
 
+  global $conn;
   $url = $_POST['addfilm'];
   $tablename = $_POST['table'];
   $client = new Snoopy();
@@ -53,22 +54,30 @@ if(!empty($_POST['addfilm'])) {
     errorLog(321, "Replace ".$film->id, "add.php", 48);
 
     $sql = "UPDATE $tablename SET img_link='".$film->img_link."', film_url='".$film->filmlink.
-           "', name='".$film->name."', englishName='".$film->englishName.
+           "', name='".$film->name."', englishName='".$conn->real_escape_string($film->englishName).
            "', directors='".$film->directors."', year='".$film->year.
            "', countries='".$film->countries."', genres='".$film->genres.
            "', rating='".$film->rating."', imdb='".$film->imdb."', runtime='".$film->runtime."' WHERE id=".$film->id;
 
     $result = $conn->query($sql);
   } else {
-    $sql = "INSERT INTO $tablename (img_link, film_url, name, englishName, directors,".
-           " year, countries, genres, rating, imdb, runtime) VALUES ('".$film->img_link.
-           "','".$film->filmlink."','".$film->name."','".$film->englishName."','".$film->directors.
+    errorLog(321, "Add ".$film->name, "add.php", 48);
+
+    $sql = "INSERT INTO `$tablename` (`img_link`, `film_url`, `name`, `englishName`, `directors`,".
+           " `year`, `countries`, `genres`, `rating`, `imdb`, `runtime`) VALUES ('".$film->img_link.
+           "','".$film->filmlink."','".$film->name."','".$conn->real_escape_string($film->englishName)."','".$film->directors.
            "','".$film->year."','".$film->countries."','".$film->genres."','".$film->rating.
            "','".$film->imdb."','".$film->runtime."')";
 
+    errorLog(111, "SQL: ".$sql, "add.php", 71);
+    errorLog(289, "Error: ".var_export($conn, true), "add.php", 72);
+
     $result = $conn->query($sql);
+
     $film->id = $conn->insert_id;
+    errorLog(321, "Result ".$film->id, "add.php", 48);
   }
 
   printRow($film);
+  $conn->close();
 }
